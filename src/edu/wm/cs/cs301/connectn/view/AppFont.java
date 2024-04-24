a921364a3ba688437019e0bf1f6f8331f1ac4993
James Elmore <email@jameselmore.org>
2024-04-23 21:56:37 -0400

    Added font constants

---
+++ src/edu/wm/cs/cs301/connectn/view/AppFont.java
@@ -0,0 +1,15 @@
+package edu.wm.cs.cs301.connectn.view;
+
+import java.awt.*;
+
+public enum AppFont {
+    TITLE(new Font("Dialog", Font.BOLD, 36)),
+    TEXT(new Font("Dialog", Font.PLAIN, 16)),
+    FOOTER(new Font("Dialog", Font.PLAIN, 12));
+
+    public final Font font;
+
+    private AppFont(Font font) {
+        this.font = font;
+    }
+}
